cef47428259fa7fa8db993ba148b40c0fa9864b8
Danielle Vass <danielle.vass@bitrise.io>
2021-04-28 11:27:59 +0100

    Add ApplicationVersionNameDataFormatterTest

--- trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/application/ApplicationVersionNameDataFormatterTest.java
+++ trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/application/ApplicationVersionNameDataFormatterTest.java
@@ -2,7 +2,7 @@ package io.bitrise.trace.data.management.formatter.application;
 
 import org.junit.Test;
 
-import io.bitrise.trace.data.collector.application.ApplicationVersionCodeDataCollector;
+import io.bitrise.trace.data.collector.application.ApplicationVersionNameDataCollector;
 import io.bitrise.trace.data.dto.Data;
 import io.bitrise.trace.data.dto.FormattedData;
 import io.bitrise.trace.data.management.formatter.BaseDataFormatterTest;
@@ -11,22 +11,22 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * Unit tests for {@link ApplicationVersionCodeDataFormatter}.
+ * Unit tests for {@link ApplicationVersionNameDataFormatter}.
  */
-public class ApplicationVersionCodeDataFormatterTest extends BaseDataFormatterTest {
+public class ApplicationVersionNameDataFormatterTest extends BaseDataFormatterTest {
 
-    final String resourceLabel = "app.build";
-    final String versionCode = "123";
+    final String resourceLabel = "app.version";
+    final String versionName = "1.2.3";
 
     @Test
     public void formatData() {
-        final Data data = new Data(ApplicationVersionCodeDataCollector.class);
-        data.setContent(versionCode);
-        final ApplicationVersionCodeDataFormatter formatter = new ApplicationVersionCodeDataFormatter();
+        final Data data = new Data(ApplicationVersionNameDataCollector.class);
+        data.setContent(versionName);
+        final ApplicationVersionNameDataFormatter formatter = new ApplicationVersionNameDataFormatter();
         final FormattedData[] formattedData = formatter.formatData(data);
         assertEquals(1, formattedData.length);
         assertNotNull(formattedData[0].getResourceEntity());
         assertEquals(resourceLabel, formattedData[0].getResourceEntity().getLabel());
-        assertEquals(versionCode, formattedData[0].getResourceEntity().getValue());
+        assertEquals(versionName, formattedData[0].getResourceEntity().getValue());
     }
 }
