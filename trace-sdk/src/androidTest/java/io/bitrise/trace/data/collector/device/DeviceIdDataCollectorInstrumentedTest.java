ce49962451bf9e2abeb3ab8248066b105c95fd1a
Danielle Vass <danielle.vass@bitrise.io>
2021-05-11 15:20:34 +0100

    Update DeviceIdDataCollectorInstrumentedTest

--- trace-sdk/src/androidTest/java/io/bitrise/trace/data/collector/device/DeviceIdDataCollectorInstrumentedTest.java
+++ trace-sdk/src/androidTest/java/io/bitrise/trace/data/collector/device/DeviceIdDataCollectorInstrumentedTest.java
@@ -8,11 +8,14 @@ import io.bitrise.trace.data.collector.BaseDataCollectorInstrumentedTest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Instrumented tests for {@link DeviceIdDataCollector}.
  */
 public class DeviceIdDataCollectorInstrumentedTest extends BaseDataCollectorInstrumentedTest {
+
+    final DeviceIdDataCollector deviceIdDataCollector = new DeviceIdDataCollector(context);
 
     /**
      * Verifies that when {@link DeviceIdDataCollector#collectData()} is called, the content of the returned
@@ -31,9 +34,13 @@ public class DeviceIdDataCollectorInstrumentedTest extends BaseDataCollectorInst
      */
     @Test
     public void collectData_deviceIdShouldNotChange() {
-        final DeviceIdDataCollector deviceIdDataCollector = new DeviceIdDataCollector(context);
         final String expectedValue = (String) deviceIdDataCollector.collectData().getContent();
         final String actualValue = (String) deviceIdDataCollector.collectData().getContent();
         assertThat(actualValue, is(expectedValue));
+    }
+
+    @Test
+    public void getPermissions() {
+        assertArrayEquals(new String[0], deviceIdDataCollector.getPermissions());
     }
 }
