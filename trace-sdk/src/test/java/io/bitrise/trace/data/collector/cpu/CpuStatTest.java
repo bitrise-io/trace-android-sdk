d13164bff13e42c06b45124a04b7b9a53a0af1ba
Danielle Vass <danielle.vass@bitrise.io>
2021-05-12 15:49:39 +0100

    Add tests for CpuUsageData

---
+++ trace-sdk/src/test/java/io/bitrise/trace/data/collector/cpu/CpuStatTest.java
@@ -0,0 +1,43 @@
+package io.bitrise.trace.data.collector.cpu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for {@link CpuUsageData.CpuStat}
+ */
+public class CpuStatTest {
+    final CpuUsageData.CpuStat cpuStat = new CpuUsageData.CpuStat(
+            1, 2, 3, 4, 5, 6, 7, 8 );
+
+    @Test
+    public void toString_shouldMatch() {
+        final String expected = "CpuStat{user=1.0, system=3.0, nice=2.0, idle=4.0, ioWait=5.0, irq=6.0, softIrq=7.0, steal=8.0}";
+        assertEquals(expected, cpuStat.toString());
+    }
+
+    @Test
+    public void equals_sameObject() {
+        assertTrue(cpuStat.equals(cpuStat));
+    }
+
+    @Test
+    public void equals_notCpuStat() {
+        assertFalse(cpuStat.equals("cpu_stat"));
+    }
+
+    @Test
+    public void equals_differentCpuStat() {
+        final CpuUsageData.CpuStat differentCpuStat = new CpuUsageData.CpuStat(
+                10, 20, 30, 40, 50, 60, 70, 80 );
+        assertFalse(cpuStat.equals(differentCpuStat));
+    }
+
+    @Test
+    public void hashCode_shouldMatch() {
+        assertEquals(1573154561, cpuStat.hashCode());
+    }
+}
---
+++ trace-sdk/src/test/java/io/bitrise/trace/data/collector/cpu/PidCpuStatWithUptimeTest.java
@@ -0,0 +1,55 @@
+package io.bitrise.trace.data.collector.cpu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for {@link CpuUsageData.PidCpuStatWithUptime}
+ */
+public class PidCpuStatWithUptimeTest {
+
+    private final CpuUsageData.PidCpuStat pidCpuStat = new CpuUsageData.PidCpuStat(
+            1, 2, 3, 4, 5 );
+    private final CpuUsageData.PidCpuStatWithUptime pidCpuStatWithUptime =
+            new CpuUsageData.PidCpuStatWithUptime(pidCpuStat, 10, 20 );
+
+    @Test
+    public void toString_shouldMatchWithContent() {
+        final String expected = "PidCpuStatWithUptime{pidCpuStat=AppCpuStat{utime=1.0, stime=2.0, cutime=3.0, cstime=4.0, starttime=5.0}, uptime=10.0, elapsedSeconds=20.0}";
+        assertEquals(expected, pidCpuStatWithUptime.toString());
+    }
+
+    @Test
+    public void toString_shouldMatchNoContent() {
+        final String expected = "PidCpuStatWithUptime{pidCpuStat=AppCpuStat{utime=0.0, stime=0.0, cutime=0.0, cstime=0.0, starttime=0.0}, uptime=0.0, elapsedSeconds=0.0}";
+        final CpuUsageData.PidCpuStatWithUptime emptyPidCpuStatWithUptime = new CpuUsageData.PidCpuStatWithUptime();
+        assertEquals(expected, emptyPidCpuStatWithUptime.toString());
+    }
+
+    @Test
+    public void equals_sameObject() {
+        assertTrue(pidCpuStatWithUptime.equals(pidCpuStatWithUptime));
+    }
+
+    @Test
+    public void equals_notPidCpuStat() {
+        assertFalse(pidCpuStatWithUptime.equals("pid_cpu_stat"));
+    }
+
+    @Test
+    public void equals_differentPidCpuStatWithUptime() {
+        final CpuUsageData.PidCpuStat differentPidCpuStat = new CpuUsageData.PidCpuStat(
+                10,20,30,40,50 );
+        final CpuUsageData.PidCpuStatWithUptime differentPidCpuStatWithUptime =
+                new CpuUsageData.PidCpuStatWithUptime(differentPidCpuStat, 100, 200 );
+        assertFalse(pidCpuStatWithUptime.equals(differentPidCpuStatWithUptime));
+    }
+
+    @Test
+    public void hashCode_shouldMatch() {
+        assertEquals(1975361854, pidCpuStatWithUptime.hashCode());
+    }
+}
