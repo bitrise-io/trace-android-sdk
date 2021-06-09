77d02e58fb3d0e2da56444a36c96c872218aaa80
Danielle Vass <danielle.vass@bitrise.io>
2021-06-09 16:43:55 +0100

    fix lint warnings

--- trace-sdk/src/main/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatter.java
+++ trace-sdk/src/main/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatter.java
@@ -6,8 +6,17 @@ import io.bitrise.trace.data.dto.CrashData;
 import io.bitrise.trace.data.dto.CrashReport;
 import java.util.Map;
 
+/**
+ * Formats {@link CrashData} objects into {@link CrashReport} objects.
+ */
 public class ExceptionDataFormatter {
 
+  /**
+   * Formats a {@link CrashData} object into a {@link CrashReport} object.
+   *
+   * @param crashData the original crash data.
+   * @return the formatted {@link CrashReport} object.
+   */
   public static CrashReport formatCrashData(final @NonNull CrashData crashData) {
 
     final String throwable = Throwables.getStackTraceAsString(crashData.getThrowable());
--- trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/crash/CrashFormatterTestProvider.java
+++ trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/crash/CrashFormatterTestProvider.java
@@ -5,16 +5,16 @@ package io.bitrise.trace.data.management.formatter.crash;
  */
 public class CrashFormatterTestProvider {
 
-  final static String expectedStringifiedStackTrace = "class1.method1(file1:1)\n" +
-      "class2.method2(filename2:2)\n" +
-      "class3.method3(filename3:3)";
+  static final String expectedStringifiedStackTrace = "class1.method1(file1:1)\n"
+      + "class2.method2(filename2:2)\n"
+      + "class3.method3(filename3:3)";
 
   static StackTraceElement[] createStackTraceElements() {
     final StackTraceElement[] list = new StackTraceElement[3];
 
-    list[0] = new StackTraceElement("class1", "method1", "file1" ,1);
-    list[1] = new StackTraceElement("class2", "method2", "filename2" ,2);
-    list[2] = new StackTraceElement("class3", "method3", "filename3" ,3);
+    list[0] = new StackTraceElement("class1", "method1", "file1", 1);
+    list[1] = new StackTraceElement("class2", "method2", "filename2", 2);
+    list[2] = new StackTraceElement("class3", "method3", "filename3", 3);
 
     return list;
   }
