b8b1c60e2cdb8adc3310cd5f21df24423533cc3e
Danielle Vass <danielle.vass@bitrise.io>
2021-06-09 17:02:12 +0100

    fix failing ExceptionDataFormatterTest

--- trace-sdk/src/main/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatter.java
+++ trace-sdk/src/main/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatter.java
@@ -1,7 +1,6 @@
 package io.bitrise.trace.data.management.formatter.crash;
 
 import androidx.annotation.NonNull;
-import com.google.common.base.Throwables;
 import io.bitrise.trace.data.dto.CrashData;
 import io.bitrise.trace.data.dto.CrashReport;
 import java.util.Map;
@@ -19,7 +18,8 @@ public class ExceptionDataFormatter {
    */
   public static CrashReport formatCrashData(final @NonNull CrashData crashData) {
 
-    final String throwable = Throwables.getStackTraceAsString(crashData.getThrowable());
+    final String throwable = StackTraceElementUtil
+            .createStringifiedStackTrace(crashData.getThrowable().getStackTrace());
     final Map<String, String> threads =
         StackTraceElementUtil.createStringifiedReport(crashData.getAllStackTraces());
 
--- trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatterTest.java
+++ trace-sdk/src/test/java/io/bitrise/trace/data/management/formatter/crash/ExceptionDataFormatterTest.java
@@ -16,49 +16,17 @@ import org.mockito.Mockito;
  */
 public class ExceptionDataFormatterTest {
 
-  private final String expectedThrowableString = "java.lang.Throwable\n"
-      + "\tat io.bitrise.trace.data.management.formatter.crash.ExceptionDataFormatterTest"
-      + ".formatCrashData_noMessageInThrowable(ExceptionDataFormatterTest.java:59)\n"
-      + "\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n"
-      + "\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke"
-      + "(NativeMethodAccessorImpl.java:62)\n"
-      + "\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke"
-      + "(DelegatingMethodAccessorImpl.java:43)\n"
-      + "\tat java.base/java.lang.reflect.Method.invoke(Method.java:566)\n"
-      + "\tat org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)"
-      + "\n"
-      + "\tat org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)\n"
-      + "\tat org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)\n"
-      + "\tat org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)\n"
-      + "\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\n"
-      + "\tat org.junit.runners.BlockJUnit4ClassRunner$1.evaluate(BlockJUnit4ClassRunner.java:100)"
-      + "\n"
-      + "\tat org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:366)\n"
-      + "\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:103)\n"
-      + "\tat org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:63)\n"
-      + "\tat org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)\n"
-      + "\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)\n"
-      + "\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)\n"
-      + "\tat org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)\n"
-      + "\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)\n"
-      + "\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\n"
-      + "\tat org.junit.runners.ParentRunner.run(ParentRunner.java:413)\n"
-      + "\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\n"
-      + "\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner"
-      + ".java:69)\n"
-      + "\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner"
-      + ".java:33)\n"
-      + "\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:220)\n"
-      + "\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:53)\n";
-
   @Test
-  public void formatCrashData_noMessageInThrowable() {
+  public void formatCrashData() {
     final Map<Thread, StackTraceElement[]> allStackTraces = new HashMap<>();
     final Thread mockThread = Mockito.mock(Thread.class);
     allStackTraces.put(mockThread, CrashFormatterTestProvider.createStackTraceElements());
-    final CrashData input = new CrashData(new Throwable(), allStackTraces);
+    final Throwable mockThrowable = Mockito.mock(Throwable.class);
+    final CrashData input = new CrashData(mockThrowable, allStackTraces);
 
     when(mockThread.getId()).thenReturn(12345L);
+    when(mockThrowable.getStackTrace())
+        .thenReturn(CrashFormatterTestProvider.createStackTraceElements());
 
     final CrashReport result = ExceptionDataFormatter.formatCrashData(input);
 
@@ -68,7 +36,7 @@ public class ExceptionDataFormatterTest {
     expectedThreads.put("12345", CrashFormatterTestProvider.expectedStringifiedStackTrace);
 
     assertEquals(result.getThreads(), expectedThreads);
-    assertEquals(expectedThrowableString, result.getThrowable());
+    assertEquals(result.getThrowable(), CrashFormatterTestProvider.expectedStringifiedStackTrace);
   }
 
 
